8c8df9cf0be13fcfa1e120cd966dc8049feeb76f
AsmaRW <asma.rw09@gmail.com>
2021-05-27 15:54:27 +0200

    res

--- front-student/student/src/App.js
+++ front-student/student/src/App.js
@@ -1,12 +1,29 @@
-import './App.css';
-import instance from './instance'
+// import { useState, useEffect } from 'react'
+import { Switch, Route, BrowserRouter } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import StudentList from './views/StudentList'
 
 function App() {
   return (
-    <div>
-        
-    </div>
+    <BrowserRouter>
+      <div>
+        <h1>Students</h1>
+
+
+        <Switch>
+          <Route path="/" exact>
+            <StudentList />
+
+          </Route>
+
+          <Route path="studentAdded" exact>
+
+          </Route>
+
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
-
 export default App;
