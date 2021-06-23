e675fab2c008990bed84444a2b2948b7871a677e
AsmaRW <asma.rw09@gmail.com>
2021-06-23 15:43:03 +0200

    log

---
+++ login_frontend/login/src/pages/Signup.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom'
+import React from 'react'
+
+function Signup() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Signup
